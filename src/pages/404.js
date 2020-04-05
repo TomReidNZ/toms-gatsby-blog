361354731221ff4e8bb73f7eff7135cdae786be8
tomreidnz <treid.nz@gmail.com>
2020-04-05 18:18:38 -0300

    clean up

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import { graphql } from "gatsby";
 import { Container } from "react-bootstrap";
-import config from "../../data/SiteConfig";
-
 import Layout from "../layout";
-import SEO from "../components/SEO";
-import SocialMediaAbout from "../components/SocialMedia/about-social.js";
 
-// import ImageHero from "../components/ImageHero";
-// import TopicList from "../components/TopicList";
 import "./about.scss";
 
 class NotFound extends Component {
