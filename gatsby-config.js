30731ccf08d6be342a0c6792739c586ccb59e589
tomreidnz <treid.nz@gmail.com>
2020-04-05 19:05:36 -0300

    revert manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,18 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "minimal-ui",
-        icons: `/src/favicon.ico`
+        icons: [
+          {
+            src: "/logos/logo-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logos/logo-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
       }
     },
     "gatsby-plugin-offline",
